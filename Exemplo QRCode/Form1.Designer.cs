831c286d94676e747628abfd7f21441ddaac5359
CorsiIsa <icorsi15@gmail.com>
2023-06-26 17:55:18 -0300

    Criando um QR code

---
+++ Exemplo QRCode/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿
+namespace Exemplo_QRCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnCode1 = new System.Windows.Forms.Button();
+            this.btnCode2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(42, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(242, 260);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(337, 34);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(242, 260);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnCode1
+            // 
+            this.btnCode1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCode1.Location = new System.Drawing.Point(42, 331);
+            this.btnCode1.Name = "btnCode1";
+            this.btnCode1.Size = new System.Drawing.Size(242, 51);
+            this.btnCode1.TabIndex = 2;
+            this.btnCode1.Text = "&Gerar QRCode";
+            this.btnCode1.UseVisualStyleBackColor = true;
+            this.btnCode1.Click += new System.EventHandler(this.btnCode1_Click);
+            // 
+            // btnCode2
+            // 
+            this.btnCode2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCode2.Location = new System.Drawing.Point(337, 331);
+            this.btnCode2.Name = "btnCode2";
+            this.btnCode2.Size = new System.Drawing.Size(242, 51);
+            this.btnCode2.TabIndex = 3;
+            this.btnCode2.Text = "&Gerar QRCode ";
+            this.btnCode2.UseVisualStyleBackColor = true;
+            this.btnCode2.Click += new System.EventHandler(this.btnCode2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 424);
+            this.Controls.Add(this.btnCode2);
+            this.Controls.Add(this.btnCode1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnCode1;
+        private System.Windows.Forms.Button btnCode2;
+    }
+}
+
---
+++ Exemplo QRCode/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QRCoder;
+
+namespace Exemplo_QRCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCode1_Click(object sender, EventArgs e)
+        {
+            GerarQRCode1();
+        }
+
+        private void GerarQRCode1()
+        {
+            string Nome = "Gabriel Alves";
+
+            QRCodeGenerator qrGeneretor = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGeneretor.CreateQrCode(Nome, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            pictureBox1.Image = qrCode.GetGraphic(4);
+        }
+
+        private void btnCode2_Click(object sender, EventArgs e)
+        {
+            GerarQRCode2();
+        }
+
+        private void GerarQRCode2()
+        {
+            string Nome = "Isabelle Corsi";
+
+            QRCodeGenerator qrGeneretor = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGeneretor.CreateQrCode(Nome, QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+            pictureBox2.Image = qrCode.GetGraphic(4);
+        }
+    }
+}
